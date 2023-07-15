8be6fded446ed8afe6e36d7f3dd4449770111194
kenpaicat <133065911+kenpaicat@users.noreply.github.com>
2023-07-15 20:08:53 +0200

    fix broken links

--- crates/laguna-backend-api/src/lib.rs
+++ crates/laguna-backend-api/src/lib.rs
@@ -1,6 +1,6 @@
 //! API handlers, states and errors.
-#![doc(html_logo_url = "https://sloveniaengineering.github.io/logo.png")]
-#![doc(html_favicon_url = "https://sloveniaengineering.github.io/favicon.ico")]
+#![doc(html_logo_url = "https://sloveniaengineering.github.io/laguna-backend/logo.png")]
+#![doc(html_favicon_url = "https://sloveniaengineering.github.io/laguna-backend/favicon.ico")]
 #![doc(issue_tracker_base_url = "https://github.com/SloveniaEngineering/laguna-backend")]
 pub mod error;
 pub mod login;
