d1ee67c9a57730c017aef268c14d07edfdcc11a1
kenpaicat <133065911+kenpaicat@users.noreply.github.com>
2023-07-15 20:20:53 +0200

    fix broken links again

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-#![doc(html_logo_url = "https://sloveniaengineering.github.io/logo.png")]
-#![doc(html_favicon_url = "https://sloveniaengineering.github.io/favicon.ico")]
+#![doc(html_logo_url = "https://sloveniaengineering.github.io/laguna-backend/logo.png")]
+#![doc(html_favicon_url = "https://sloveniaengineering.github.io/laguna-backend/favicon.ico")]
 #![doc(issue_tracker_base_url = "https://github.com/SloveniaEngineering/laguna-backend")]
 use actix_cors::Cors;
 
