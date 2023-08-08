0621e0eea3bf94e3a460c90222dffd97bc761c07
kenpaicat <133065911+kenpaicat@users.noreply.github.com>
2023-08-08 10:35:53 +0200

    mlock/mprotect secrets from DB in memory using

--- crates/laguna-backend-api/src/register.rs
+++ crates/laguna-backend-api/src/register.rs
@@ -6,7 +6,7 @@ use argon2::{
 };
 
 use laguna_backend_dto::register::RegisterDTO;
-use laguna_backend_model::user::User;
+use laguna_backend_model::user::{User, UserSafe};
 
 use sqlx::PgPool;
 
@@ -34,41 +34,46 @@ pub async fn register(
     register_dto: Json<RegisterDTO>,
     pool: web::Data<PgPool>,
 ) -> Result<HttpResponse, APIError> {
-    let fetched_user = sqlx::query_as!(
-        User,
-        r#"
-            SELECT id,
-                   username,
-                   email,
-                   password,
-                   first_login,
-                   last_login,
-                   avatar_url,
-                   salt,
-                   role AS "role: _",
-                   behaviour AS "behaviour: _",
-                   is_active,
-                   has_verified_email,
-                   is_history_private,
-                   is_profile_private
-            FROM "User" 
-            WHERE username = $1 OR email = $2"#,
-        register_dto.username,
-        register_dto.email
-    )
-    .fetch_optional(pool.get_ref())
-    .await?;
+    // In own scope for faster drop of fetched_user, because we don't need it much.
+    {
+        let fetched_user = sqlx::query_as!(
+            User,
+            r#"
+                SELECT id,
+                    username,
+                    email,
+                    password,
+                    first_login,
+                    last_login,
+                    avatar_url,
+                    salt,
+                    role AS "role: _",
+                    behaviour AS "behaviour: _",
+                    is_active,
+                    has_verified_email,
+                    is_history_private,
+                    is_profile_private
+                FROM "User" 
+                WHERE username = $1 OR email = $2"#,
+            register_dto.username,
+            register_dto.email
+        )
+        .fetch_optional(pool.get_ref())
+        .await?
+        .map(UserSafe::from);
 
-    if let Some(_) = fetched_user {
-        return Ok(HttpResponse::AlreadyReported().finish());
+        if let Some(_) = fetched_user {
+            return Ok(HttpResponse::AlreadyReported().finish());
+        }
     }
 
+    // https://github.com/SloveniaEngineering/laguna-backend/issues/54#issuecomment-1645126931
     let argon_context = Argon2::new(
         Algorithm::Argon2id,
         Version::V0x13,
         ParamsBuilder::new()
             .p_cost(1)
-            .m_cost(12288)
+            .m_cost(12288) // 12MiB in kibibytes
             .t_cost(3)
             .build()
             .unwrap(),
--- crates/laguna-backend-model/src/user.rs
+++ crates/laguna-backend-model/src/user.rs
@@ -4,9 +4,9 @@ use crate::role::Role;
 use actix_jwt_auth_middleware::FromRequest;
 use chrono::{DateTime, Utc};
 
+use secrecy::Secret;
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
-// use secrecy::SerializableSecret;
 /// User DB object.
 /// Not to be confused with [`UserDTO`] used for API.
 #[derive(Serialize, Deserialize, Debug, Clone, FromRequest, sqlx::FromRow)]
@@ -30,4 +30,42 @@ pub struct User {
     pub has_verified_email: bool,
     pub is_history_private: bool,
     pub is_profile_private: bool,
+}
+
+pub struct UserSafe {
+    pub id: Uuid,
+    pub username: String,
+    pub email: Secret<String>,
+    pub password: Secret<String>,
+    pub first_login: DateTime<Utc>,
+    pub last_login: DateTime<Utc>,
+    pub avatar_url: Option<String>,
+    pub salt: Secret<String>,
+    pub role: Role,
+    pub behaviour: Behaviour,
+    pub is_active: bool,
+    pub has_verified_email: bool,
+    pub is_history_private: bool,
+    pub is_profile_private: bool,
+}
+
+impl From<User> for UserSafe {
+    fn from(user: User) -> Self {
+        Self {
+            id: user.id,
+            username: user.username,
+            email: Secret::new(user.email),
+            password: Secret::new(user.password),
+            first_login: user.first_login,
+            last_login: user.last_login,
+            avatar_url: user.avatar_url,
+            salt: Secret::new(user.salt),
+            role: user.role,
+            behaviour: user.behaviour,
+            is_active: user.is_active,
+            has_verified_email: user.has_verified_email,
+            is_history_private: user.is_history_private,
+            is_profile_private: user.is_profile_private,
+        }
+    }
 }
