305966e5af9609cfc12cef309074f43297bf0aa3
kenpaicat <133065911+kenpaicat@users.noreply.github.com>
2023-09-14 12:57:37 +0200

    SQLX_OFFLINE=true for tests

--- scripts/test-win.ps1
+++ scripts/test-win.ps1
@@ -1,7 +1,10 @@
+$PSDefaultParameterValues.Remove("env:SQLX_OFFLINE")
 $PSDefaultParameterValues.Remove("env:DATABASE_URL")
 
+
 $PSDefaultParameterValues = @{
-    "env:DATABASE_URL"="postgres://postgres:postgres@127.0.0.1:5432/laguna_dev_db"
+  "env:SQLX_OFFLINE"="true"
+  "env:DATABASE_URL"="postgres://postgres:postgres@127.0.0.1:5432/laguna_dev_db"
 }
 
 # Run all tests and show their output
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-export DATABASE_URL=${DATABASE_URL:-postgres://postgres:postgres@127.0.0.1:5432/laguna_dev_db}
+export SQLX_OFFLINE=true
+export DATABASE_URL=${DATABASE_URL:-postgres://postgres:postgres@127.0.0.1:5432/laguna_db}
 
 # Run all tests and show their output
 cargo test --all --features testx -- --nocapture
