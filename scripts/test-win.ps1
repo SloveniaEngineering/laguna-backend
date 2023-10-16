89bcb4908390a515e590ebd0f1931d991c774fe7
kenpaicat <133065911+kenpaicat@users.noreply.github.com>
2023-10-16 13:14:03 +0200

    v1.0.0-beta initial commit, refresh materialized views in transaction

--- scripts/test-win.ps1
+++ scripts/test-win.ps1
@@ -6,4 +6,4 @@ $PSDefaultParameterValues = @{
 }
 
 # Run all tests and show their output
-cargo test --all --features testx -- --nocapture
+$env:DATABASE_URL="postgres://postgres:postgres@127.0.0.1:5432/laguna_dev_db"; cargo test --all --features testx -- --nocapture
